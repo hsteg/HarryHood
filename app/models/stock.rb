73daa4fd032ba25b7fe55a9a1667680e112dbd18
Harrison Steg <hstegaa@gmail.com>
2019-01-22 13:46:48 -0500

    Add auth pattern to user model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -10,5 +10,5 @@
 #
 
 class Stock < ApplicationRecord
-    
+        
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,40 @@
 #
 
 class User < ApplicationRecord
+    attr_reader :password
+
+    validates :username, :email, :first_name, :last_name, :account_balance, :password_digest, :session_token, presence: true
+    valdates :username, :email, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil unless user && user.is_password?(password)
+        return user
+    end
+
+    def reset_session_token
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    private 
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
     
+
+
 end
