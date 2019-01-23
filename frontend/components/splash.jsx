5e2d6b6160b377d3a5bb136fd9db5087f3e5acb3
Harrison Steg <hstegaa@gmail.com>
2019-01-23 10:51:39 -0500

    add logout button, have splash page correctly render either login/signup buttons or logout buttons based on session info

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../actions/session_actions';
 
 
 class Splash extends React.Component {
-    
+
     render() {
+        const correctAuthButtons = this.props.currentUser ? (
+            <div>
+                <h2>Welcome to HarryHood, {this.props.currentUser.first_name}</h2>
+                <button onClick={this.props.logout}>Log Out :(</button>
+            </div>
+        ) : (
+                <div>
+                    <Link to="/login">Login</Link>
+                    <Link to="/signup">Sign Up</Link>
+                </div>
+            );
+
         return (
-        <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-        </div>
+            <div>
+                {correctAuthButtons}
+            </div>
         );
     }
 }
 
-export default Splash;
+const msp = (state) => {
+    return {
+        currentUser: state.entities.users[state.session.id]
+    };
+}
+
+const mdp = (dispatch) => {
+    return {
+        logout: () => dispatch(logout()),
+    };
+}
+
+export default connect(msp, mdp)(Splash);
