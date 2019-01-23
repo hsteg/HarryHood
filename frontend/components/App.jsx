38be011457b41f8966383f3a9970639b46f48d78
Harrison Steg <hstegaa@gmail.com>
2019-01-23 11:12:15 -0500

    implemented auth routes, preventing logged in users from accessing signup or login pages (redirect to root)

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,12 +3,14 @@ import LoginForm from './login_form';
 import SignUpForm from './signup_form';
 import { Route } from "react-router-dom";
 import Splash from './splash';
+import { AuthRoute } from '../util/route_util';
+
 
 const App = () => {
     return (
         <div>
-            <Route path="/login" component={LoginForm} />
-            <Route path="/signup" component={SignUpForm} />
+            <AuthRoute path="/login" component={LoginForm} />
+            <AuthRoute path="/signup" component={SignUpForm} />
             <Route exact path="/" component={Splash} />
         </div>
     );
