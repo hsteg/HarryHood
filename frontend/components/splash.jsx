0698d3bb85c18002cde5bf3cc14e4c28941b37ba
Harrison Steg <hstegaa@gmail.com>
2019-01-23 17:47:07 -0500

    complete splash page, for now

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -13,18 +13,25 @@ class Splash extends React.Component {
           <div className="splash-navbar">
             <div className="splash-logo-name-holder">
               <Link to="/"><img className="splash-logo-image" src={window.logoURL} /></Link>
-              <span>HarryHood</span>
+              <div className="splash-logo-text">HarryHood</div>
             </div>
             <div className="splash-navbar-left-links-holder">
-              <h3>Content 1</h3>
-              <h3>Content 2</h3>
-              <h3>Content 3</h3>
-              <h3>Content 4</h3>
-
+              <div className="splash-navbar-left-link-item">
+                <a href="#">Content 1</a>   
+              </div>
+              <div className="splash-navbar-left-link-item">
+              <a href="#">Content 2</a>
+              </div>
+              <div className="splash-navbar-left-link-item">
+              <a href="#">Content 3</a>
+              </div>
+              <div className="splash-navbar-left-link-item">
+              <a href="#">Content 4</a>
+              </div>
             </div>
             <div className="splash-navbar-right-buttons-holder">
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
+              <Link to="/login" className="splash-login">Login</Link>
+              <Link to="/signup"><button className="splash-signup-button">Sign Up</button></Link>
             </div>
           </div>
         </div>
@@ -33,9 +40,9 @@ class Splash extends React.Component {
           <section className="splash-section1">
             <div className="splash-content-section1">
               <div className="splash-content-section1-left">
-                <h1>blah blah header</h1>
-                <h2>blah blah subheaer</h2>
-                <p>paragraph text</p>
+                <h1>Invest for Free</h1>
+                <h3>Invest in stocks and ETFs, all commission-free, all not actually real transactions right from your desktop.</h3>
+                <Link to="/signup"><button className="splash-signup-button">Sign Up</button></Link>
               </div>
               <div className="splash-content-section1-right">
                 <img className="splash-content-section1-image" src={window.splashMockURL} />
@@ -44,12 +51,9 @@ class Splash extends React.Component {
           </section>
           <section className="splash-section2">
             <div className="splash-section2-text">
-              <span>Mobile-friendly layout, coming soon.</span>
+              Mobile-friendly layout, coming soon.
             </div>
             <img className="splash-section2-img" src={window.dogURL} />
-          </section>
-          <section className="splash-section3">
-
           </section>
         </div>
       </div>
