cab855e045f740c65b7ad24b13e58e854b6809a8
Harrison Steg <hstegaa@gmail.com>
2019-01-24 16:34:02 -0500

    complete css skeleton for dashboard

--- frontend/components/dashboard.jsx
+++ frontend/components/dashboard.jsx
@@ -5,26 +5,158 @@ import { logout } from '../actions/session_actions';
 
 class Dashboard extends React.Component {
 
-    render() {
-        return (
-            <div>
-                <h2>Welcome to HarryHood, {this.props.currentUser.first_name}</h2>
-                <button onClick={this.props.logout}>Log Out :(</button>
+  render() {
+    return (
+      <div className="dashboard-main">
+        <div className="dashboard-nav-holder">
+          <div className="dashboard-navbar">
+            <div className="dashboard-nav-logo-search">
+              <div className="dashboard-nav-logo">
+                Logo here
+              </div>
+              <div className="dashboard-nav-searchbar">
+                Searchbar here
+              </div>
             </div>
-        );
-    }
+            <div className="dashboard-nav-right-links">
+              <div className="dashboard-nav-right-links-link-item">Home</div>
+              <div className="dashboard-nav-right-links-link-item">Notifications</div>
+              <div className="dashboard-nav-right-links-link-item">Account</div>
+            </div>
+          </div>
+        </div>
+        <div className="dashboard-content-container">
+          <div className="dashboard-content">
+            <div className="dashboard-content-left-main-container">
+              <div className="dashboard-content-left-main">
+                <section className="dashboard-content-graph-container">
+                  <div className="dashboard-content-graph-header-container">
+                    <div className="dashboard-content-graph-header-portfolio-value">
+                      porfolio value here
+                    </div>
+                    <div className="dashboard-content-graph-header-change-container">
+                      day change
+                    </div>
+                    <div className="dashboard-content-graph-header-change-container-afterhours">
+                      after hours change
+                    </div>
+                  </div>
+                  <div className="dashboard-content-graph">
+                    graph here
+                  </div>
+                  <nav className="dashboard-content-graph-timeline-selector">
+
+                  </nav>
+                </section>
+                <section className="dashboard-content-news-container">
+                  <div className="dashboard-content-news-header-container">
+                    <div className="dashboard-content-news-header">
+                      NEWS goes here
+                    </div>
+                  </div>
+                  <div className="dashboard-content-news-list-container">
+                    <div className="dashboard-content-news-item-container">
+                      <div className="dashboard-content-news-item">
+                          News Item
+                      </div>
+                      <div className="dashboard-content-news-item">
+                          News Item
+                      </div>
+                      <div className="dashboard-content-news-item">
+                          News Item
+                      </div>
+                      <div className="dashboard-content-news-item">
+                          News Item
+                      </div>
+                      <div className="dashboard-content-news-item">
+                          News Item
+                      </div>
+                    </div>
+                  </div>
+                </section>
+
+              </div>
+            </div>
+            <div className="dashboard-content-right-main-container">
+              <div className="dashboard-content-right-main">
+                <section className="dashboard-content-right-users-stocks">
+                  <div className="dashboard-content-right-users-stocks-header">
+                    <div className="dashboard-content-users-stocks-header-title">
+                      User stocks
+                    </div>
+                    <div className="dashboard-content-users-stocks-header-button">
+                      Button
+                    </div>
+                  </div>
+                  <div className="dashboard-content-right-users-stocks-stockitem">
+                    <div className="dashboard-content-right-stockitem-name-container">
+                      <div className="dashboard-content-right-stockitem-name">
+                      </div>
+                      <div className="dashboard-content-right-stockitem-numshares">
+                      </div>
+                    </div>
+                    <div className="dashboard-content-right-stockitem-graph-container">
+                      <div className="dashboard-content-right-stockitem-graph">
+                        graph
+                      </div>
+                    </div>
+                    <div className="dashboard-content-right-stockitem-price-container">
+                      <div className="dashboard-content-right-stockitem-price">
+
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section className="dashboard-content-right-users-watchlist">
+                  <div className="dashboard-content-right-users-watchlist-header">
+                    <div className="dashboard-content-users-watchlist-header-title">
+                      User watchlist
+                    </div>
+                    <div className="dashboard-content-users-watchlist-header-button">
+                      Button
+                    </div>
+                  </div>
+                  <div className="dashboard-content-right-users-watchlist-watchitem">
+                    <div className="dashboard-content-right-watchitem-name-container">
+                      <div className="dashboard-content-right-watchitem-name">
+                      </div>
+                    </div>
+                    <div className="dashboard-content-right-watchitem-graph-container">
+                      <div className="dashboard-content-right-watchitem-graph">
+                        graph
+                      </div>
+                    </div>
+                    <div className="dashboard-content-right-watchitem-price-container">
+                      <div className="dashboard-content-right-watchitem-price">
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
+
+
+
+
+
+
 const msp = (state) => {
-    return {
-        currentUser: state.entities.users[state.session.id]
-    };
+  return {
+    currentUser: state.entities.users[state.session.id]
+  };
 };
 
 const mdp = (dispatch) => {
-    return {
-        logout: () => dispatch(logout()),
-    };
+  return {
+    logout: () => dispatch(logout()),
+  };
 }
 
 export default connect(msp, mdp)(Dashboard);
