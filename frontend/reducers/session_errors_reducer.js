db1849f6c685b450d3c09675aba62850666a98e4
Harrison Steg <hstegaa@gmail.com>
2019-01-23 19:00:18 -0500

    modify value of state.errors.session to be an array

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -4,7 +4,7 @@ const sessionErrorsReducer = (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_SESSION_ERRORS:
-            return action.errors;
+            return Object.values(action.errors);
         case RECEIVE_CURRENT_USER:
             return [];
         case CLEAR_SESSION_ERRORS:
