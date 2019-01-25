14875910620ee1d56c5e3e3820c577ffda842968
Harrison Steg <hstegaa@gmail.com>
2019-01-25 15:08:05 -0500

    Add user watch actions

---
+++ frontend/actions/user_watch_actions.js
@@ -0,0 +1,17 @@
+import * as APIUtil from '../util/user_watch_util';
+export const RECEIVE_USER_WATCHES = "RECEIVE_USER_WATCHES";
+
+export const getUserWatches = (user) => dispatch => {
+  return APIUtil.getUserWatches(user).then(
+    watches => {
+      return dispatch(receiveUserWatches(watches));
+    }
+  );
+};
+
+const receiveUserWatches = (watches) => {
+  return {
+    type: RECEIVE_USER_WATCHES,
+    watches
+  }
+}
