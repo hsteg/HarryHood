157458724dc7e1f6983e83289b6e131cc3ca145c
Harrison Steg <hstegaa@gmail.com>
2019-01-27 19:29:10 -0500

    add nice styling for dropdown menu in navbar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -5,6 +5,28 @@ import { logout } from '../actions/session_actions';
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dropdown: "loggedin-nav-right-links-account-dropdown-container-hidden"
+    }
+    this.navOpen = this.navOpen.bind(this);
+    this.navClose = this.navClose.bind(this);
+  }
+
+  navOpen() {
+    this.setState({
+      dropdown: "loggedin-nav-right-links-account-dropdown-container",
+    });
+    document.addEventListener('click', this.navClose, false);
+    document.getElementById('dropdown').addEventListener('click',function(e) {
+      e.stopPropagation();
+    }, true);
+  }
+
+  navClose() {
+    this.setState({
+      dropdown: "loggedin-nav-right-links-account-dropdown-container-hidden",
+    });
+    document.removeEventListener('click', this.navClose);
   }
 
   render() {
@@ -24,14 +46,31 @@ class Navbar extends React.Component {
           <div className="loggedin-nav-right-links-link-item">Home</div>
           <div className="loggedin-nav-right-links-link-item">Notifications</div>
           <div className="loggedin-nav-right-links-link-item">
-            <p>Account</p>
-            <div className="loggedin-nav-right-links-account-dropdown-container">
+            <button onClick={this.navOpen}>Account</button>
+            <div className={this.state.dropdown}  id="dropdown" >
               <div className="loggedin-nav-right-links-account-dropdown-menu">
-                
+                <div className="loggedin-nav-right-links-account-dropdown-menu-header">
+                  <h3>
+                    {this.props.currentUser.first_name} {this.props.currentUser.last_name}
+                  </h3>
+                  <div className="loggedin-nav-right-links-account-dropdown-menu-header-finances">
+                    <div className="loggedin-nav-right-links-dropdown-finances-porfolio-value">
+                      porfolio value
+                    </div>
+                    <div className="loggedin-nav-right-links-dropdown-finances-cash-balance">
+                      {this.props.currentUser.cash_balance}
+                    </div>
+                  </div>
+                </div>
+                <div className="loggedin-nav-right-links-account-dropdown-menu-content-2">
+                  content 2
+                </div>
+                <div className="loggedin-nav-right-links-account-dropdown-menu-content-3">
+                  <button onClick={this.props.logout}>Log Out :(</button>
+                </div>
               </div>
             </div>
           </div>
-          <button onClick={this.props.logout}>Log Out :(</button>
         </div>
       </div>
     );
