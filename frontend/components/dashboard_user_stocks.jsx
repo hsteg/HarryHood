4e77665207cabfb1730245fc28b7cce32f58cf14
Harrison Steg <hstegaa@gmail.com>
2019-01-27 22:30:56 -0500

    more css styling

--- frontend/components/dashboard_user_stocks.jsx
+++ frontend/components/dashboard_user_stocks.jsx
@@ -7,22 +7,24 @@ class DashboardUserStockList extends React.Component {
   }
 
   render() {
-    if (this.props.loading) { return (<h1>loading :)</h1>); } 
-    const {stocks} = this.props;
+    if (this.props.loading) { return (<h1>loading :)</h1>); }
+    const { stocks } = this.props;
     const lis = Object.values(this.props.transactions).map(transaction => {
-      
+
       return (
-        <li key={transaction.id}>
-          <div className="userstock-stock-symbol">
-            {stocks[transaction.stock_id].symbol}
+        <div className="list-item">
+          
+            <div className="userstock-stock-symbol">
+              {stocks[transaction.stock_id].symbol}
+            </div>
+            <div className="userstock-stock-graph">
+              graph here
           </div>
-          <div className="userstock-stock-graph">
-            graph here
-          </div>
-          <div className="userstock-stock-price">
-            {stocks[transaction.stock_id].quote.latestPrice}
-          </div>
-        </li>
+            <div className="userstock-stock-price">
+              {stocks[transaction.stock_id].quote.latestPrice}
+            </div>
+
+        </div>
       );
     }
 
--- frontend/components/dashboard_watchlist.jsx
+++ frontend/components/dashboard_watchlist.jsx
@@ -7,22 +7,24 @@ class DashboardWatchlist extends React.Component {
   }
 
   render() {
-    if (this.props.loading) { return (<h1>loading :)</h1>); } 
-    const {stocks} = this.props;
+    if (this.props.loading) { return (<h1>loading :)</h1>); }
+    const { stocks } = this.props;
     const lis = Object.values(this.props.watches).map(watch => {
-      
+
       return (
-        <li key={watch.id}>
-          <div className="watchlist-stock-symbol">
-            {stocks[watch.stock_id].symbol}
+        <div className="list-item">
+   
+            <div className="watchlist-stock-symbol">
+              {stocks[watch.stock_id].symbol}
+            </div>
+            <div className="watchlist-stock-graph">
+              graph here
           </div>
-          <div className="watchlist-stock-graph">
-            graph here
-          </div>
-          <div className="watchlist-stock-price">
-            {stocks[watch.stock_id].quote.latestPrice}
-          </div>
-        </li>
+            <div className="watchlist-stock-price">
+              {stocks[watch.stock_id].quote.latestPrice}
+            </div>
+          
+        </div>
       );
     }
 
