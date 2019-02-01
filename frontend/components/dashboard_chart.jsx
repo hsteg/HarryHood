e3f94741c7de9395ebf51d70e50e819a09698ea3
Harrison Steg <hstegaa@gmail.com>
2019-01-31 21:17:54 -0500

    stock charts visuals completely working for everything but Day view on dashboard

--- frontend/components/dashboard_chart.jsx
+++ frontend/components/dashboard_chart.jsx
@@ -7,9 +7,9 @@ class DashboardChart extends React.Component {
   constructor(props) {
     super(props);
     this.parseChartData = this.parseChartData.bind(this);
-    this.allChartData = this.allChartData.bind(this);
+    this.greaterThanOneDayChartData = this.greaterThanOneDayChartData.bind(this);
     this.chartColor = this.chartColor.bind(this);
-    this.oneYearChartData = this.oneYearChartData.bind(this);
+    this.chartRange = this.chartRange.bind(this);
   }
 
   parseChartData() {
@@ -18,82 +18,88 @@ class DashboardChart extends React.Component {
       case "1D":
         return { data: this.oneDayChartData(), width: 675, refColor: "gray" };
       case "1W":
-        return { data: this.greaterThanOneDayChartData("1W"), width: 675, refColor: "transparent" };
+        return { data: this.greaterThanOneDayChartData(5), 
+                color: this.chartColor(5), 
+                range: this.chartRange(5),  
+                width: 675, 
+                refColor: "transparent" };
       case "1M":
-        return { data: this.greaterThanOneDayChartData("1M"), width: 675, refColor: "transparent" };
+        return { data: this.greaterThanOneDayChartData(22), 
+                color: this.chartColor(22), 
+                range: this.chartRange(22), 
+                width: 675, 
+                refColor: "transparent" };
       case "3M":
-        return { data: this.greaterThanOneDayChartData("3M"), width: 675, refColor: "transparent" };
+        return { data: this.greaterThanOneDayChartData(66), 
+                color: this.chartColor(66), 
+                range: this.chartRange(66), 
+                width: 675, 
+                refColor: "transparent" };
       case "1Y":
-        return { data: this.oneYearChartData(), color: this.chartColor("1Y"), width: 675, refColor: "transparent" };
+        return { data: this.greaterThanOneDayChartData(264), 
+                color: this.chartColor(264), 
+                range: this.chartRange(264), 
+                width: 675, 
+                refColor: "transparent" };
       case "ALL":
-        return { data: this.allChartData(), color: this.chartColor("ALL"), width: 675, refColor: "transparent" };
+        return { data: this.greaterThanOneDayChartData(this.props.chartData.length), 
+                color: this.chartColor(this.props.chartData.length), 
+                range: this.chartRange(this.props.chartData.length), 
+                width: 675, 
+                refColor: "transparent" };
       default:
         return null;
     }
   }
 
-  allChartData() {
+  greaterThanOneDayChartData(length) {
     if (!this.props.chartData) { return []; }
-    const allChartData = [];
-    this.props.chartData.forEach(dataPoint => {
-      let dpObject = {};
-      dpObject['Date'] = (dataPoint.date).toString();
-      dpObject['Value'] = dataPoint.total_portfolio_value;
-      allChartData.push(dpObject);
-    }
-    );
-    return allChartData;
-  }
-
-  oneYearChartData() {
-    if (!this.props.chartData) { return []; }
-    let beginningIdx = this.props.chartData.length - 264;
+    let beginningIdx = this.props.chartData.length - length;
     beginningIdx < 0 ? beginningIdx = 0 : beginningIdx;
     const chartData = this.props.chartData.slice(beginningIdx)
-    const oneYearChartData = [];
+    const greaterThanOneDayChartData = [];
     chartData.forEach(dataPoint => {
       let dpObject = {};
       dpObject['Date'] = (dataPoint.date).toString();
       dpObject['Value'] = dataPoint.total_portfolio_value;
-      oneYearChartData.push(dpObject);
+      greaterThanOneDayChartData.push(dpObject);
     }
     );
-    return oneYearChartData;
+    return greaterThanOneDayChartData;
   }
 
-
-
-  chartColor(range) {
-    const lastEl = this.props.chartData[this.props.chartData.length-1]
+  chartColor(length) {
     let first;
-    let last = lastEl.total_portfolio_value;
-    switch (range) {
-      case "1D":
-        return "blue";
-      case "1W":
-      case "1M":
-      case "3M":
-      case "1Y":
-        let beginningIdx = this.props.chartData.length - 264;
-        if (beginningIdx < 0) {
-          first = this.props.chartData[0].total_portfolio_value;
-        } else {
-          first = this.props.chartData[beginningIdx].total_portfolio_value;
-        }
-        return (first <= last) ? ("#21ce99") : ("#f45531");
-      case "ALL":
-        first = this.props.chartData[0].total_portfolio_value;
-        last = lastEl.total_portfolio_value;
-        return (first <= last) ? ("#21ce99") : ("#f45531");
-      default:
-        return null;
+    let last = this.props.chartData[this.props.chartData.length - 1].total_portfolio_value;
+    let beginningIdx = this.props.chartData.length - length;
+
+    if (beginningIdx < 0) {
+      first = this.props.chartData[0].total_portfolio_value;
+    } else {
+      first = this.props.chartData[beginningIdx].total_portfolio_value;
     }
+    return (first <= last) ? ("#21ce99") : ("#f45531");
+  }
+
+  chartRange(length) {
+    let first;
+
+    let last = this.props.chartData[this.props.chartData.length - 1].total_portfolio_value;
+
+    let beginningIdx = this.props.chartData.length - length;
+
+    if (beginningIdx < 0) {
+      first = this.props.chartData[0].total_portfolio_value;
+    } else {
+      first = this.props.chartData[beginningIdx].total_portfolio_value;
+    }
+    return [first, last];
   }
 
   render() {
-    if(this.props.loading.userPortfolioDataLoading) {return (<img className="right-col-loading-img" src={window.loadingIMG} />); };
-    const lastDataPoint = this.props.chartData[this.props.chartData.length-1].total_portfolio_value
-    
+    if (this.props.loading.userPortfolioDataLoading) { return (<img className="right-col-loading-img" src={window.loadingIMG} />); };
+    const lastDataPoint = this.props.chartData[this.props.chartData.length - 1].total_portfolio_value
+
     const allData = this.parseChartData();
     return (
       <div className="dashboard-chart">
@@ -102,14 +108,14 @@ class DashboardChart extends React.Component {
             <h1>${lastDataPoint}</h1>
           </div>
           <div className="chart-header-change-value">
-            +$420.69 (4.20%) 
+            +$420.69 (4.20%)
           </div>
         </div>
         <div className="chart-actual-chart">
-        <LineChart width={allData.width} height={190} data={allData.data}>
-            <Line type="monotone" dataKey="Value" stroke={allData.color} dot={false} strokeWidth={2}   />
+          <LineChart width={allData.width} height={190} data={allData.data}>
+            <Line type="monotone" dataKey="Value" stroke={allData.color} dot={false} strokeWidth={2} />
             <XAxis dataKey="Date" hide={true} />
-            <YAxis type="number" domain={[20000, 60000]} hide={true} />
+            <YAxis type="number" domain={allData.range} hide={true} />
             <Tooltip contentStyle={{ backgroundColor: 'transparent', border: '0' }} />
           </LineChart>
         </div>
