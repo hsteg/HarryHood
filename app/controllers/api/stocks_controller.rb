ec2489ce8a32a94407e5b23e0bc5ec7d5a6d1af0
Harrison Steg <hstegaa@gmail.com>
2019-01-31 22:38:44 -0500

    stock backend search query working for two columns based on input

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -8,6 +8,14 @@ class Api::StocksController < ApplicationController
     render 'api/stocks/show'
   end
 
+  def search
+    key = params[:search_string]
+    @stock = Stock.where("symbol LIKE '%#{key}%' OR name LIKE '%#{key}%'")
+    debugger
+    render 'api/stocks/show'
+
+  end
+
   private
   def stock_params 
     params.require(:stock).permit(:symbol, :name)
