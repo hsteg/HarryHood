7e3ed327c5b9ff099b8c0709afe4a801d8713f21
Harrison Steg <hstegaa@gmail.com>
2019-02-07 17:31:19 -0500

    fixed bug where dashboard was crashing

--- frontend/components/dashboard.jsx
+++ frontend/components/dashboard.jsx
@@ -48,8 +48,8 @@ class Dashboard extends React.Component {
 
   displayUserWatchList(){
     const { userWatchListLoading, dashboardStocksLoading } = this.props.loading;
-    
-    if ( userWatchListLoading || dashboardStocksLoading) {
+    const notEnoguhStocks = Object.values(this.props.stocks).length === Object.values(this.props.userWatches).length
+    if ( userWatchListLoading || dashboardStocksLoading || !notEnoguhStocks) {
       return (<img className="right-col-loading-img" src={window.loadingIMG} />);
     } else {
       return (<DashboardWatchlist watches={this.props.userWatches} stocks={this.props.stocks} watchListValue={this.state.watchListValue} />);
